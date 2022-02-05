4559cf1d2091a5e993ac8c353fa5d4f07e769cd2
nikolashn <nehn.2019@gmail.com>
2022-02-05 00:11:49 +0000

    Update config.mk to link to the correct GLFW lib

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ VERSION = 1.0.0
 # C compiler
 CC = cc
 # C compiler flags
-CFLAGS = -std=c11 -O2 -Wall -Wshadow -lglfw3 -lrt -lm -ldl -lc -lpthread -lX11 -lGL -lGLEW 
+CFLAGS = -std=c11 -O2 -Wall -Wshadow -lglfw -lrt -lm -ldl -lc -lpthread -lX11 -lGL -lGLEW 
 # Path
 PPATH = /usr
